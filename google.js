01bbd61b4f38a4eb64ac4dcf28c9b6b84a2b4615
João Ferreira <joao.ferreira@wig.pt>
2018-02-26 13:02:20 +0000

    testing google

---
+++ google.js
@@ -0,0 +1,29 @@
+var Nightmare = require('nightmare');
+var vo = require('vo');
+
+
+function Status() {
+    var scraper = new Nightmare({
+        show: false, webPreferences: {
+            images: false
+        }
+    });
+
+    scraper
+        .goto('https://www.google.com')
+
+        .evaluate(function () {
+           return document.querySelectorAll('div')[0].innerText
+        })
+
+        .then(function(log){
+            console.log(log)
+        })
+
+}
+
+vo(Status)(function (err, titles) {
+
+
+    console.log('hello');
+});
