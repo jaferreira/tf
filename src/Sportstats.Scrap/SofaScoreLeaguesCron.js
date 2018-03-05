4f465f29d5e41d15c246a9992a3774edd324516d
João Ferreira <joao.ferreira@wig.pt>
2018-03-05 16:17:20 +0000

    bug on catch

--- src/Sportstats.Scrap/SofaScoreLeaguesCron.js
+++ src/Sportstats.Scrap/SofaScoreLeaguesCron.js
@@ -1,9 +1,22 @@
 var cron = require('node-cron'),
     leaguesJob = require('./SofaScoreLeagues');
 
+tryCount = 0;
 
+
+
+process.on('unhandledRejection', (reason, p) => {
+    if (tryCount <= 5) {
+        console.log('retry - ' + tryCount)
+        tryCount++;
+        leaguesJob.scrapLeague();
+    }
+    else {
+        //TODO reporting error
+    }
+});
 
 cron.schedule('*/5 * * * *', function () {
     console.log('hey');
-    leaguesJob.scrapLeague(); 
+    leaguesJob.scrapLeague();
 }, null, true);
