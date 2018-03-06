ea92d00873e858b17330f9c0f6b024da0d525dfa
Luis Antunes <luis.antunes@wig.pt>
2018-03-06 17:52:26 +0000

    Add swagger endpoint.

--- src/Sportstats.API/Api.js
+++ src/Sportstats.API/Api.js
@@ -10,11 +10,13 @@ var logger = require('./Logger.js'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
     expressMetrics = require('express-metrics'),
+    swaggerUi = require('swagger-ui-express'),
+    swaggerJSDoc = require('swagger-jsdoc'),
 
     // Models
     TeamToScrap = require('./Models/TeamToScrap'),
     TeamInfo = require('./Models/TeamInfo');
-    LeagueToScrap = require('./Models/LeagueToScrap'),
+LeagueToScrap = require('./Models/LeagueToScrap'),
     LeagueInfo = require('./Models/LeagueInfo');
 
 
@@ -31,11 +33,48 @@ mongoose.connect(mongoConnString, function (err) {
         console.log("The aplication is terminating...");
         process.exit();
     }
-    
+
+    // Swagger
+    // Swagger definition
+    // You can set every attribute except paths and swagger
+    // https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md
+    var swaggerDefinition = {
+        info: { // API informations (required)
+            title: 'Sportstats', // Title (required)
+            version: '0.1.0', // Version (required)
+            description: 'Sportstats API', // Description (optional)
+        },
+        host: 'localhost:3000', // Host (optional)
+        basePath: '/', // Base path (optional)
+    };
+
+    // Options for the swagger docs
+    var options = {
+        // Import swaggerDefinitions
+        swaggerDefinition: swaggerDefinition,
+        // Path to the API docs
+        apis: ['./Routes/*.js']
+    };
+
+    // Initialize swagger-jsdoc -> returns validated swagger spec in json format
+    var swaggerSpec = swaggerJSDoc(options);
+
+    // Serve swagger docs the way you like (Recommendation: swagger-tools)
+    app.get('/api-docs.json', function (req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
+
+    // Swagger UI
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
     // Metrics Configuration
     app.use(expressMetrics({
         port: metricsPort
     }));
+
+
 
     app.use(cors());
 
@@ -56,7 +95,6 @@ mongoose.connect(mongoConnString, function (err) {
     scrapRoutes(app);
     var infoRoutes = require('./Routes/InfoRoutes');
     infoRoutes(app);
-
 
     app.listen(port);
     logger.info('Sportstats API server started on: ' + port);
--- src/Sportstats.API/Routes/ScrapRoutes.js
+++ src/Sportstats.API/Routes/ScrapRoutes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     // Get pending teams to be scraped
     app.route('/scrap/teams/pending')
         .get(teamsScrapController.get_pending_teams_to_scrap);
-    
+
     // Create new team to be scraped
     app.route('/scrap/teams')
         .post(teamsScrapController.create_team_to_scrap);
@@ -15,22 +15,112 @@ module.exports = function (app) {
     // Save scraped team info (upsert)
     app.route('/scrap/teams/:team')
         .post(teamsScrapController.save_team_scrap_info);
-    
+
 
 
     // Leagues
     var leaguesScrapController = require('../Controllers/LeaguesScrapController');
 
-    // Get pending leagues to be scraped
+    /**
+     * @swagger
+     * definitions:
+     *   Provider:
+     *     type: object
+     *     required:
+     *       - provider
+     *       - link
+     *     properties:
+     *       - provider:
+     *          type: string
+     *       - link:
+     *          type: string
+     *   LeagueInfo:
+     *     type: object
+     *     required:
+     *       - provider
+     *       - link
+     *     properties:
+     *       - name:
+     *          type: string
+     *       - permalink:
+     *          type: string
+     *       - country:
+     *          type: string
+     *       - titleHolder:
+     *          type: string
+     *       - mostTitlesNumber:
+     *          type: number
+     *   LeagueToScrap:
+     *     type: object
+     *     required:
+     *       - permalink
+     *       - name
+     *       - country
+     *       - providers
+     *     properties:
+     *       permalink:
+     *         type: string
+     *       name:
+     *         type: string
+     *       country:
+     *         type: string
+     *       providers:
+     *         type: array
+     *         items:
+     *             $ref: '#/definitions/Provider'
+     */
+
+    /**
+     * @swagger
+     * /scrap/leagues/pending:
+     *   get:
+     *     description: Returns leagues
+     *     produces:
+     *      - application/json
+     *     responses:
+     *       200:
+     *         description: leagues
+     *         schema:
+     *           type: array
+     *           items:
+     *             $ref: '#/definitions/LeagueToScrap'
+     */
     app.route('/scrap/leagues/pending')
         .get(leaguesScrapController.get_pending_leagues_to_scrap);
-        
-    // Create new league to be scraped
+
+    /**
+     * @swagger
+     * /scrap/leagues:
+     *   post:
+     *     description: Create new league to be scraped
+     *     produces:
+     *      - application/json
+     *     responses:
+     *       200:
+     *         description: leagueToScrap
+     *         schema:
+     *             $ref: '#/definitions/LeagueToScrap'
+     */
     app.route('/scrap/leagues')
         .post(leaguesScrapController.create_league_to_scrap);
-    
-    // Save scraped league info (upsert)
+
+    /**
+     * @swagger
+     * /scrap/leagues/league:
+     *   post:
+     *     description: Save scraped league info (upsert)
+     *     produces:
+     *      - application/json
+     *     parameters:
+     *       - name: league
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: League
+     *         schema:
+     *             $ref: '#/definitions/LeagueInfo'
+     */
     app.route('/scrap/leagues/:league')
         .post(leaguesScrapController.save_league_scrap_info);
-    
+
 };
