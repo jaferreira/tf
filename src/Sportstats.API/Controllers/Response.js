de315e843dc3fab09b9cca24666c8680313d3085
Luis Antunes <luis.antunes@wig.pt>
2018-03-09 18:18:27 +0000

    Update response object for API.

--- src/Sportstats.API/Controllers/Response.js
+++ src/Sportstats.API/Controllers/Response.js
@@ -1,26 +1,32 @@
-exports.successResponse = function () {
-    return {
-        success: true
-    };
- };
-exports.successResponse = function (resultObj) {
-    return {
-        success: true,
-        resultObj: resultObj
-    };
- };
 
-exports.successResponse = function (resultObj, message) {
-    return {
-        success: true,
-        resultObj: resultObj,
-        message: message
-    };
- };
+ var response = {
+	successResponse: function () {
+        return {
+            success: true
+        };
+     },
+     
+     successResponse = function (resultObj) {
+        return {
+            success: true,
+            resultObj: resultObj
+        };
+     },
 
-exports.errorResponse = function (message) {
-    return {
-        success: false,
-        message: message
-    };
- };
+     successResponse = function (resultObj, message) {
+        return {
+            success: true,
+            resultObj: resultObj,
+            message: message
+        };
+     },
+    
+     errorResponse = function (message) {
+        return {
+            success: false,
+            message: message
+        };
+     }
+};
+
+module.exports = response;
