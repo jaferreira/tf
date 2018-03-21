dd348a82e7aaf8868b8d39b98220de36fbad5718
Luis Antunes <luis.antunes@wig.pt>
2018-03-21 16:30:54 +0000

    Update teams dictionary.

--- src/Sportstats.API/Controllers/teamsController.js
+++ src/Sportstats.API/Controllers/teamsController.js
@@ -259,7 +259,487 @@ class TeamsController extends BaseController {
             whoTeamId: '22',
             whoTeamName: 'Bradford',
             whoTeamLink: 'https://www.whoscored.com/Teams/22/Show/England-Bradford',
-        }];
+        },
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '46',
+
+            sofaTeamName: 'Blackburn Rovers',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/blackburn-rovers/46',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '158',
+
+            whoTeamName: 'Blackburn',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/158/Show/England-Blackburn',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '82',
+
+            sofaTeamName: 'Shrewsbury Town',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/shrewsbury-town/82',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '207',
+
+            whoTeamName: 'Shrewsbury',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/207/Show/England-Shrewsbury',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '49',
+
+            sofaTeamName: 'Wigan',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/wigan-athletic/49',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '194',
+
+            whoTeamName: 'Wigan',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/194/Show/England-Wigan',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '13',
+
+            sofaTeamName: 'Rotherham United',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/rotherham-united/13',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '210',
+
+            whoTeamName: 'Rotherham',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/210/Show/England-Rotherham',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '85',
+
+            sofaTeamName: 'Scunthorpe United',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/scunthorpe-united/85',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '186',
+
+            whoTeamName: 'Scunthorpe',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/186/Show/England-Scunthorpe',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '71',
+
+            sofaTeamName: 'Plymouth Argyle',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/plymouth-argyle/71',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '212',
+
+            whoTeamName: 'Plymouth',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/212/Show/England-Plymouth',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '54',
+
+            sofaTeamName: 'Peterborough United',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/peterborough-united/54',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '215',
+
+            whoTeamName: 'Peterborough',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/215/Show/England-Peterborough',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '2',
+
+            sofaTeamName: 'Portsmouth',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/portsmouth/2',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '169',
+
+            whoTeamName: 'Portsmouth',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/169/Show/England-Portsmouth',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '47',
+
+            sofaTeamName: 'Charlton Athletic',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/charlton-athletic/47',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '160',
+
+            whoTeamName: 'Charlton',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/160/Show/England-Charlton',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '76',
+
+            sofaTeamName: 'Bristol Rovers',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/bristol-rovers/76',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '185',
+
+            whoTeamName: 'Bristol Rovers',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/185/Show/England-Bristol-Rovers',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '22',
+
+            sofaTeamName: 'Bradford City',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/bradford-city/22',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '22',
+
+            whoTeamName: 'Bradford',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/22/Show/England-Bradford',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '20',
+
+            sofaTeamName: 'Gillingham',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/gillingham/20',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '191',
+
+            whoTeamName: 'Gillingham',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/191/Show/England-Gillingham',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '99',
+
+            sofaTeamName: 'Southend United',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/southend-united/99',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '208',
+
+            whoTeamName: 'Southend',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/208/Show/England-Southend',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '67',
+
+            sofaTeamName: 'Blackpool',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/blackpool/67',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '93',
+
+            whoTeamName: 'Blackpool',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/93/Show/England-Blackpool',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '95',
+
+            sofaTeamName: 'Oxford United',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/oxford-united/95',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '197',
+
+            whoTeamName: 'Oxford',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/197/Show/England-Oxford',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '26',
+
+            sofaTeamName: 'Walsall',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/walsall/26',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '173',
+
+            whoTeamName: 'Walsall',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/173/Show/England-Walsall',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '84',
+
+            sofaTeamName: 'Doncaster Rovers',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/doncaster-rovers/84',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '910',
+
+            whoTeamName: 'Doncaster',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/910/Show/England-Doncaster',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '23957',
+
+            sofaTeamName: 'AFC Wimbledon',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/afc-wimbledon/23957',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '5955',
+
+            whoTeamName: 'AFC Wimbledon',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/5955/Show/England-AFC-Wimbledon',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '65',
+
+            sofaTeamName: 'Oldham Athletic',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/oldham-athletic/65',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '190',
+
+            whoTeamName: 'Oldham',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/190/Show/England-Oldham',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '23959',
+
+            sofaTeamName: 'Fleetwood Town',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/fleetwood-town/23959',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '3936',
+
+            whoTeamName: 'Fleetwood',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/3936/Show/England-Fleetwood',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '57',
+
+            sofaTeamName: 'Northampton Town',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/northampton-town/57',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '316',
+
+            whoTeamName: 'Northampton',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/316/Show/England-Northampton',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '4',
+
+            sofaTeamName: 'Milton Keynes Dons',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/milton-keynes-dons/4',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '28',
+
+            whoTeamName: 'Milton Keynes Dons',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/28/Show/England-Milton-Keynes-Dons',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '103',
+
+            sofaTeamName: 'Rochdale AFC',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/rochdale-afc/103',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '202',
+
+            whoTeamName: 'Rochdale',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/202/Show/England-Rochdale',
+
+        },
+
+        {
+
+            sofaProviderName: 'SofaScore',
+
+            sofaTeamId: '52',
+
+            sofaTeamName: 'Bury',
+
+            sofaTeamLink: 'https://www.sofascore.com/team/football/bury/52',
+
+            whoScoredProviderName: 'WhoScored',
+
+            whoTeamId: '187',
+
+            whoTeamName: 'Bury',
+
+            whoTeamLink: 'https://www.whoscored.com/Teams/187/Show/England-Bury',
+
+        }
+        ];
 
         var teamsData = req.body;
 
@@ -273,7 +753,7 @@ class TeamsController extends BaseController {
                 ids.push(team.permalink);
                 logger.info(' » (' + team.country + ') ' + team.name + ' » ' + team.permalink);
             }
-            
+
 
             // SETTING GAMES TO BE SCRAPED
             if (team.nextGame) {
